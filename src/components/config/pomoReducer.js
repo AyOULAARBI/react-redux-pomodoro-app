0574d636b7350b0db93f9a51008f08811873db4e
AyOULAARBI <ayoub6oulaarbi6@gmail.com>
2023-01-21 20:52:10 +0100

    made some changes in the reducer file but I am still having a problem when I try to change the state

--- src/components/config/pomoReducer.js
+++ src/components/config/pomoReducer.js
@@ -5,21 +5,23 @@ import { createSlice } from "@reduxjs/toolkit";
 //       const [current,setCurrent] = useState(fields[activeBtn]);
 //       const [percentage,setPercentage] = useState(100);
 //       const [time,setTime] = useState(current.minutes*60);
+let initialState ={
+  periods:[{type:'sprint',time:25},{type:'short break',time:5},{type:'long break',time:15}],
+  isActive:false,
+  activeBtn:0,
+  get current(){return this.periods[this.activeBtn]},
+  percentage:100,
+  get time(){return this.current.time * 60},
+}
 let PomodoroSlice = createSlice({
   name:"pomodoro",
-  initialState:{
-    periods:[{type:'sprint',time:25},{type:'short break',time:5},{type:'long break',time:15}],
-    isActive:false,
-    activeBtn:0,
-    get current(){return this.periods[this.activeBtn]},
-    percentage:100,
-    get time(){return this.current.minutes * 60}
-  },
+  initialState:initialState,
   reducers:{
     setActiveBtn: (state,action)=> ({...state,activeBtn : action.payload}),
-    
+    decr: (state)=> ({...state,time: 2}),
+    setStatus: (state)=>({...state,isActive: ! state.isActive }),
   }
 })
 
-export let {setActiveBtn} = PomodoroSlice.actions;
+export let {setActiveBtn,decr,setStatus} = PomodoroSlice.actions;
 export default PomodoroSlice.reducer;
