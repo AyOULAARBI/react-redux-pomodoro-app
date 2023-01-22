b6c81f26dbf2f5d4af4ee924ca4d759d77ae3d8b
AyOULAARBI <ayoub6oulaarbi6@gmail.com>
2023-01-22 17:31:32 +0100

    changed the ugly style of the settings component,now it's better than it was lol

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import { useSelector,useDispatch } from "react-redux";
 import { setShowSettings,setPeriods } from "./config/pomoReducer";
 
@@ -20,20 +21,62 @@ function Settings() {
     }
     
   return (
-    <div>
+    <Container>
         <form onSubmit={handleSubmit}>
             {
                 periods.map((element,index)=>
                 <div key={index}>
-                    <label>{element.type}</label>
+                    <label>{element.type}:</label>
                     <input type='number' name={element.type} value={element.time} onChange={()=>handleChange(event,element)}/>
                 </div>)
             }
             <button type="submit">save</button>
 
         </form>
-    </div>
+    </Container>
   )
 }
 
-export default Settings
+export default Settings;
+
+const Container = styled.div`
+height:100vh;
+display:flex;
+justify-content:center;
+align-items:center;
+form{
+    display:flex;
+justify-content:space-between;
+align-items:center;
+flex-direction:column;
+}
+form >div{
+    border: 1px solid ${props=>props.theme.colors.primary};
+    border-radius:0.2rem;
+    display:flex;
+    align-items:center;
+    flex-direction:column;
+    align-self:flex-start;
+    margin-bottom:1.2rem;
+    input{
+        all:unset;
+        text-indent:1rem;
+        color:white;
+        background-color: ${props=>props.theme.colors.primary};
+        border-radius:0.2rem;
+        font-size:1.3rem;
+        font-weight:bolder;
+        margin-top:0.2rem;
+    }
+}
+button{
+    all:unset;
+    color:white;
+    padding:0.6rem 1.1rem;
+    font-weight:bold;
+    text-align:center;
+    font-size:1.4rem;
+    border:none;
+    border-radius:1rem;
+    background-color: ${props=>props.theme.colors.primary};
+`
